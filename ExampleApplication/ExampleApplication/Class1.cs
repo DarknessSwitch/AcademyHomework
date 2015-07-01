f82dad441dbc29406ae2794b5f9dc4c974c41495
DarknessSwitch <andrewcontys@gmail.com>
2015-07-01 17:36:36 +0200

    2 : added a file for first class with no functionality

---
+++ ExampleApplication/ExampleApplication/Class1.cs
@@ -0,0 +1,14 @@
+﻿// just a class file for another branch
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApplication
+{
+    class Class1
+    {
+        
+    }
+}
