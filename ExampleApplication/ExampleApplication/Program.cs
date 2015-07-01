b33c4efc048045fde11914b5b85db6c9c328f3b0
DarknessSwitch <andrewcontys@gmail.com>
2015-07-01 17:13:16 +0200

    1 : initial project added (only year printed)

---
+++ ExampleApplication/ExampleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().OutputYear();
+        }
+        public void OutputYear()
+        {
+            Console.WriteLine(DateTime.Now.Year);
+        }
+    }
+}
